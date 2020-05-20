a4dd42ce736239d40abbf5a7135990417db55e11
coderzstas <coderz35@gmail.com>
2020-05-20 10:15:52 +0300

    pyxis : drop parts again

--- device.mk
+++ device.mk
@@ -411,10 +411,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

cabe8de1a12faa21242f226202639002deb1d29f
coderzstas <coderz35@gmail.com>
2020-05-23 17:38:10 +0300

    powerfod

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -56,6 +56,8 @@ public:
 
 private:
     sp<IXiaomiFingerprint> xiaomiFingerprintService;
+    std::mutex mCallbackLock;
+    sp<IFingerprintInscreenCallback> mCallback;
 };
 
 }  // namespace implementation
